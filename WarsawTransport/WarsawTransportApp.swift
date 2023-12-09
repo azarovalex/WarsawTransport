762f0384d395f569109ba6d3611ecce302afec6a
Alex Azarov <alex@azarov.by>
2023-12-09 16:20:21 +0100

    Initial Commit

---
+++ WarsawTransport/WarsawTransportApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarsawTransportApp.swift
+//  WarsawTransport
+//
+//  Created by Alex Azarov on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WarsawTransportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
